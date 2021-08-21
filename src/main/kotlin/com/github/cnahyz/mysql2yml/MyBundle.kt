68922697d3240703be54053ae5ee96af85a8495b
GitHub Action <action@github.com>
2021-08-21 06:21:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/cnahyz/mysql2yml/MyBundle.kt
+++ src/main/kotlin/com/github/cnahyz/mysql2yml/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cnahyz.mysql2yml
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cnahyz/mysql2yml/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cnahyz/mysql2yml/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cnahyz.mysql2yml.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cnahyz.mysql2yml.MyBundle
 
 class MyApplicationService {
 
